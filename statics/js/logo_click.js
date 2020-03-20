3cf236ec82af073f7250c8f9cba0a5c9d3078a11
shiv <shiv.s.keshari@gmail.com>
2020-03-20 11:39:24 +0530

    h1 visible on clicking logo

--- statics/js/logo_click.js
+++ statics/js/logo_click.js
@@ -4,4 +4,6 @@ function logo_hide() {
     console.log("HI");
     document.body.style.background = '#171A1C';
     $('#logo_id').hide();
+    document.getElementById("random_shit").style.display = "block";
+
 };
