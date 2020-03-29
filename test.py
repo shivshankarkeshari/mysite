eaaf6ecc275a4ecb8190363e0c350332fa047d1a
shiv <shiv.s.keshari@gmail.com>
2020-03-29 22:30:34 +0530

    remove button and did direct view map points on load

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
 # git checkout for_deployment
 
 # git push -f heroku for_deployment:master
+#
+#
+#
+# // Add button
+# var button1 = chart.chartContainer.createChild(am4core.Button);
+# button1.padding(0, 0, 0, 0);
+# button1.width = 50;
+# button1.align = "left";
+# button1.marginLeft = 10;
+#
+# button1.icon = new am4core.Sprite();
+# button1.fill = '#dc3545';
+# button1.icon.path = "M32 2a22 22 0 0 0-6 43.1L32 62l6-16.8A22 22 0 0 0 32 2zm0 34a12 12 0 1 1 12-12 12 12 0 0 1-12 12z";
+#
+# button1.events.on("hit", function() {
+#   for (i = 0; i < b.length; i++) {
+# <!--    console.log(b[i]['latitude']);-->
+#   var marker = imageSeries.mapImages.create();
+#   marker.latitude = parseFloat(b[i]['latitude']);
+#   marker.longitude = parseFloat(b[i]['longitude']);
+# }
+#
+# });
