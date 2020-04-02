6808887f68b42cfee0f9940d17bf4984069e8b47
shiv <shiv.s.keshari@gmail.com>
2020-04-02 06:14:59 +0530

    login registration added

--- stock/urls.py
+++ stock/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='stock_home_page'),
     path('feed/', views.background_theme, name='background_template'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
